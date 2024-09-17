bdeea3b8d92100710af4767898adde3e54ffd7b5
Leticia Soto <samantasoto@gmail.com>
2024-09-17 00:25:26 +0000

    Se agrega funcion randomElement() La funcion se encarga de traer un elemento ramdom de un arreglo que obtiene por parametro

--- src/app.js
+++ src/app.js
@@ -2,12 +2,8 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   function generateExcuse() {
-    //devuelva una excusa aleatoria con la siguiente estructura:
     let who = ["The dog", "My grandma", "The mailman", "My bird"];
     let action = ["ate", "peed", "crushed", "broke"];
     let what = ["my homework", "my phone", "the car"];
@@ -19,25 +15,19 @@ window.onload = function() {
       "while I was praying"
     ];
 
-    //Variables con Math.random y Math.floor: para generar números aleatorios y seleccionar elementos al azar de los array who,action,what,when.
-    let numwho = Math.floor(Math.random() * who.length);
-    //La variable randomWho se utiliza para almacenar un elemento aleatorio seleccionado del array who lo mismo con las demás variables y arrays.
-    let randomWho = who[numwho];
-    let numaction = Math.floor(Math.random() * action.length);
-    let randomAction = action[numaction];
-    let numwhat = Math.floor(Math.random() * what.length);
-    let randomWhat = what[numwhat];
-    let numwhen = Math.floor(Math.random() * when.length);
-    let randomWhen = when[numwhen];
+    let randomElement = function(arreglo) {
+      let numArreglo = Math.floor(Math.random() * arreglo.length);
+      return arreglo[numArreglo];
+    };
 
-    //Let excuse: Concatenar las variables en una sola cadena.
-    let excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
-    return excuse;
+    let randomWho = randomElement(who);
+    let randomAction = randomElement(action);
+    let randomWhat = randomElement(what);
+    let randomWhen = randomElement(when);
+
+    return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
   }
+
   let paragraph = document.getElementById("excuse");
   paragraph.innerText = generateExcuse();
-
-  // console.log("hola");
-  console.log(generateExcuse());
-  // console.log("Hello Rigo from the console!");
 };
